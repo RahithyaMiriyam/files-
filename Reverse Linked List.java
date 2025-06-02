2bf0dd6f08ed1e99022c1370ccf45c31224ad9b7
RahithyaMiriyam <rahithyamiriyam@gmail.com>
2025-06-02 14:16:49 +0530

    Add files via upload

---
+++ Reverse Linked List.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTemp = current.next; 
+            current.next = prev;              
+            prev = current;                 
+            current = nextTemp;               
+        }
+        return prev; 
+    }
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        Solution solution = new Solution();
+        ListNode reversedHead = solution.reverseList(node1);
+        printList(reversedHead); 
+}
+}
