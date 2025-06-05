47ace4778680fc3e9dda33977026a9e28f195143
RahithyaMiriyam <rahithyamiriyam@gmail.com>
2025-06-05 10:25:03 +0530

    Add files via upload

---
+++ valid parentheses.java
@@ -0,0 +1,24 @@
+
+public class Solution{
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else if (ch == '{') {
+                stack.push('}');
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) {
+                    return false; 
+                }
+            }
+        }
+        return stack.isEmpty();   
+    }
+}
+
+        
+    
